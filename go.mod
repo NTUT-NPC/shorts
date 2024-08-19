86b4ca761499860d7d9505a2ce1d70281b0966f5
Riley Ho <riley@rileychh.dev>
2024-08-19 17:36:49 +0800

    feat: initial commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/NTUT-NPC/shorts
+
+go 1.23.0
+
+require (
+	github.com/fsnotify/fsnotify v1.7.0
+	github.com/pelletier/go-toml/v2 v2.2.2
+)
+
+require golang.org/x/sys v0.24.0 // indirect
---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/fsnotify/fsnotify"
+	"github.com/pelletier/go-toml/v2"
+)
+
+type Redirects struct {
+	Temporary map[string]string
+	Permanent map[string]string
+}
+
+var redirects Redirects
+
+func main() {
+	updateRedirects()
+	go watchRedirectsFile()
+
+	http.HandleFunc("/", handleRedirect)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func updateRedirects() {
+	file, err := os.ReadFile("config/redirects.toml")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = toml.Unmarshal([]byte(file), &redirects)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Loaded %d temporary and %d permanent redirects", len(redirects.Temporary), len(redirects.Permanent))
+}
+
+func watchRedirectsFile() {
+	// Create new watcher
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	// Add config directory to watcher
+	err = watcher.Add("config")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Watch for events
+	for event := range watcher.Events {
+		if event.Has(fsnotify.Write) && event.Name == "config/redirects.toml" {
+			go updateRedirects()
+		}
+	}
+}
+
+func handleRedirect(w http.ResponseWriter, r *http.Request) {
+	slug := r.URL.Path[1:] // Remove the leading slash
+
+	log.Printf("Redirecting %s", slug)
+
+	if url, ok := redirects.Permanent[slug]; ok {
+		http.Redirect(w, r, url, http.StatusMovedPermanently)
+		return
+	}
+
+	if url, ok := redirects.Temporary[slug]; ok {
+		http.Redirect(w, r, url, http.StatusFound)
+		return
+	}
+
+	http.NotFound(w, r)
+}
