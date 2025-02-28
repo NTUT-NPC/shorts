9d5eecfa8e2bfecd5fc7bc405d8ef39321bb6c97
Riley Ho <riley@rileychh.dev>
2025-02-28 23:49:12 +0800

    fix: remove templ from dev script

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -4,8 +4,6 @@
 go mod download
 
 # Install templ and gow
-go install github.com/a-h/templ/cmd/templ@latest
 go install github.com/mitranim/gow@latest
 
-templ generate --watch &
 gow -c run cmd/shorts/main.go
